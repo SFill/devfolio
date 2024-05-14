b786d5569a2922f65299177449df5bfd365de263
n.kalgushkin <n.kalgushkin@fun-box.ru>
2024-05-14 16:14:46 +0300

    draft

---
+++ content/blog/new-site-new-post/index.md
@@ -0,0 +1,12 @@
+---
+title: My First Blog Post
+date: '2024-01-05T13:45:00.00Z'
+description: 'A post about a new site'
+---
+
+## You are cool, thank you for visiting this page
+
+This page is a first page of my new website, ta-da
+
+![Mountain](./mountain.jpg)
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -3,75 +3,60 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Nikita Kalgushkin',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Nikita Kalgushkin | Data Engineer | Python Fullstack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Data Engineer experienced building data pipelines and web services in telecom, retail, and EdTech industries.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    // author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/SFill`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/nikita-kalgushkin/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Data Engineer with 5+ years of experience building data pipelines in telecom, retail, and EdTech industries.
+    My diverse experience in data and web field allows me to choose the optimal technology for business needs to reduce costs and maximize profits. 
+    Experienced in building robust, infinity-scaled, and effective pipelines. Seeking to leverage my skills and experience to make a positive impact as a data engineer.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
-    // Optional: List your experience, they must have `name` and `description`. `link` is optional.
-    experience: [
-      {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
-    // Optional: List your skills, they must have `name` and `description`.
-    skills: [
-      {
-        name: 'Languages & Frameworks',
-        description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
-      },
-      {
-        name: 'Other',
-        description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
-      },
-    ],
+    // projects: [
+
+    // ],
+    // // Optional: List your experience, they must have `name` and `description`. `link` is optional.
+    // experience: [
+    //   {
+    //     name: 'Acme Corp',
+    //     description: 'Full-Stack Developer, February 2020 - Present',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Globex Corp',
+    //     description: 'Full-Stack Developer, December 2017 - February 2020',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'Hooli',
+    //     description: 'Full-Stack Developer, May 2015 - December 2017',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    // ],
+    // // Optional: List your skills, they must have `name` and `description`.
+    // skills: [
+    //   {
+    //     name: 'Languages & Frameworks',
+    //     description:
+    //       'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+    //   },
+    //   {
+    //     name: 'Databases',
+    //     description: 'MongoDB, PostreSQL, MySQL',
+    //   },
+    //   {
+    //     name: 'Other',
+    //     description:
+    //       'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+    //   },
+    // ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -115,7 +100,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
